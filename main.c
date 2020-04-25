54c0c86b6db632b67082e965502a533b0568980c
Igor <cheremisinig@mail.ru>
2020-04-25 14:24:20 +0700

    Change main.c

--- main.c
+++ main.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
-
 #define N_ELEMENTS(arr) (sizeof(arr) / sizeof(*(arr)))
 
 int min_element(int* arr, int size)
 {
     int min = *arr;
-
     int* it;
     for (it = arr + 1; it < arr + size; ++it) {
         if (*it < min) {
             min = *it;
         }
     }
-
     return min;
 }
-
 void print_array(int* arr, int size)
 {
     printf("Array: ");
@@ -25,15 +21,11 @@ void print_array(int* arr, int size)
     }
     printf("\n");
 }
-
 int main()
 {
     int arr[] = {3, 1, 4, 1, 5, 9, 2};
-
     const int min = min_element(arr, N_ELEMENTS(arr));
-
     print_array(arr, N_ELEMENTS(arr));
     printf("Min element: %d\n", min);
-
     return 0;
 }
